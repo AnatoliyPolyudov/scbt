2a18a7989a88c208a3651c1029530247808c71bd
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-10-16 16:07:37 +0300

    Update web_server.py
    Change web_server

--- web_server.py
+++ web_server.py
@@ -1,9 +1,14 @@
-from flask import Flask, request from callback_handler 
-import handle_callback app = Flask(__name__) 
-@app.route('/webhook', methods=['POST']) def 
-telegram_webhook():
-    data = request.json callback_data = 
-    data['callback_query']['data'] 
-    handle_callback(callback_data) return 'OK'
+from flask import Flask, request
+from callback_handler import handle_callback
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def telegram_webhook():
+    data = request.json
+    callback_data = data['callback_query']['data']
+    handle_callback(callback_data)
+    return 'OK'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
