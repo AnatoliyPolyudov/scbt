1f95f911d6fafebc709760f3ab594ba6cc8563af
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-10-16 10:55:48 +0300

    Update order_manager.py

--- order_manager.py
+++ order_manager.py
@@ -1,5 +1,6 @@
-from event_bus import subscribe def 
-handle_button_click(data):
-    print("ORDER_MANAGER: Received button click:", 
-    data)
+from event_bus import subscribe
+
+def handle_button_click(data):
+    print("ORDER_MANAGER: Received button click:", data)
+
 subscribe("BUTTON_CLICK", handle_button_click)
