6f0f305ea3ef25b30a13a97c5577a92292568879
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-10-16 16:51:08 +0300

    Update callback_handler.py
    Update callback handler

--- callback_handler.py
+++ callback_handler.py
@@ -1,5 +1,6 @@
-from event_bus import publish def 
-handle_callback(query_data):
-    print("CALLBACK_HANDLER: Received callback:", 
-    query_data) action, price = query_data.split(':')
+from event_bus import publish
+
+def handle_callback(query_data):
+    print("CALLBACK_HANDLER: Received callback:", query_data)
+    action, price = query_data.split(':')
     publish("BUTTON_CLICK", {"action": action, "price": price})
