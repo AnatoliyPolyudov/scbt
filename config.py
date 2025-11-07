8d304ee4e1051d29d62eb7e08ab3a0f26b78e580
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-11-07 10:10:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,21 @@
 # config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # загружает переменные из .env
+
 SYMBOL = "BTC/USDT:USDT"
-TF = "1m" 
+TF = "1m"
 CAPITAL = 100
 RISK_PERCENT = 1
 
-TELEGRAM_BOT_TOKEN = "TELEGRAM_BOT_TOKEN"
-TELEGRAM_CHAT_ID = "TELEGRAM_CHAT_ID"
+# Telegram
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+# Биржа OKX
+OKX_API_KEY = os.getenv("OKX_API_KEY")
+OKX_SECRET_KEY = os.getenv("OKX_SECRET_KEY")
+OKX_PASSPHRASE = os.getenv("OKX_PASSPHRASE")
 
 CANDLES_NEEDED = 3
