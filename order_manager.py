5edffdb239e0a424fe82bf85f6097950a2127f66
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-11-07 12:09:54 +0300

    Update order_manager.py

--- order_manager.py
+++ order_manager.py
@@ -1,11 +1,17 @@
+# order_manager.py
 from event_bus import subscribe
 from exchange import place_order
+from telegram import send_balance
 
 def handle_button_click(data):
     print("ORDER_MANAGER: Received button click:", data)
     
     action = data.get("action")
-    price = float(data.get("price"))
+    
+    # Для лимитных ордеров передается цена
+    price = data.get("price")
+    if price:
+        price = float(price)
 
     if action == "BUY_LIMIT":
         print(f"Placing BUY LIMIT at {price}")
@@ -13,5 +19,9 @@ def handle_button_click(data):
     elif action == "SELL_LIMIT":
         print(f"Placing SELL LIMIT at {price}")
         place_order("SELL", price)
+    elif action == "BALANCE":
+        print("Запрос баланса")
+        send_balance()  # отправляем баланс в телеграм
 
+# Подписка на событие кнопок
 subscribe("BUTTON_CLICK", handle_button_click)
